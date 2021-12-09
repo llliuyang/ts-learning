2ddfe3764d4c2e6f4c8737bcdd29df497ad438df
liuyang <1714610279@qq.com>
2021-12-09 20:35:59 +0800

    feat(Union): 增加解题方法

--- src/Union/2.PickOptional.ts
+++ src/Union/2.PickOptional.ts
@@ -5,8 +5,13 @@ export default {}
 /**
  * 题二：保留一个对象中的可选属性类型
  */
-type PickOptional<T> = Pick<T, OptionalKeys<T>>
 
+// 方法一
+type PickOptional<T> = Pick<T, OptionalKeys<T>>
+// 方法二
+type PickOptional1<T> = {
+	[K in keyof T as Omit<T, K> extends T ? K : never]: T[K]
+}
 type a1 = PickOptional<{foo: number | undefined, bar?: string, flag: boolean}>        // {bar?:string|undefined}
 type a2 = PickOptional<{foo: number, bar?: string}>                                   // {bar?:string}
 type a3 = PickOptional<{foo: number, flag: boolean}>                                  // {}
@@ -21,4 +26,9 @@ type a5 = PickOptional<{}>                                                      
  * type Pick<T, K extends keyof T> = {
  *     [P in K]: T[P]
  * }
+ *
+ * 方法二中通过直接构造来解答
+ * 用Omit剔除K属性，看看还能不能赋值给原来的类型T
+ * 如果能，则说明这个属性K是可选的，返回K
+ * 如果不能，则说明该属性是必选的
  */
--- src/Union/4.PickRequired.ts
+++ src/Union/4.PickRequired.ts
@@ -5,8 +5,12 @@ export default {}
 /**
  * 题四：保留一个对象中的必须属性
  */
-
+// 方法一
 type PickRequired<T> = Pick<T, RequiredKeys<T>>
+// 方法二
+type PickRequired1<T> = {
+	[K in keyof T as Omit<T, K> extends T ? never : K]: T[K]
+}
 
 type a1 = PickRequired<{foo: number | undefined, bar?: string, flag: boolean}>        // {foo:number|undefined,flag:boolean}
 type a2 = PickRequired<{foo: number, bar?: string}>                                   // {foo:number}
@@ -16,7 +20,7 @@ type a5 = PickRequired<{}>                                                      
 
 /**
  * 思路解析：
- * 同第二题保留可选属性一样，使用内置Pick类型即可
+ * 同第二题保留可选属性一样，使用内置Pick类型或者构造即可
  * 前四题训练的目的：
  * 1）熟悉可选属性的特点
  * 2）掌握如何如何获取可选、必选属性的方法
